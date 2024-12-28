e507f043d34534b50132da55cdc6e011c39de87a
Ghalem Salim <salimghalem40@gmail.com>
2024-12-28 15:29:58 +0100

    added navbar to Event Page and Recharts library for charts tasks

--- src/pages/Event/Event.jsx
+++ src/pages/Event/Event.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import TodoTable from '../../components/TodoTable/TodoTable';
-
+import Navbar from '../../components/Navbar/Navbar';
 const Event = () => {
   return (
-    <div>
-      <h2>To Do List</h2>
-      <p>List of all DevUp Members and Their Availability</p>
-      <TodoTable />
-    </div>
+    <>
+      <Navbar />
+      <div>
+        <h2>To Do List</h2>
+        <p>List of all DevUp Members and Their Availability</p>
+        <TodoTable />
+      </div>
+    </>
   );
 };
 
