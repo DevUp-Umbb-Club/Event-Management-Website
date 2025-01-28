91c5a1dd6e2bb3728e21f4fa5d812c75a66aae8c
Nadjoua2004 <154743720+Nadjoua2004@users.noreply.github.com>
2025-01-28 15:59:36 +0100

    most-participants-pieChart

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,24 @@ import Login from './pages/Login/Login';
 import './App.css';
 import ResetPassword from './pages/ResetPassword/ResetPassword';
 import Error404 from './pages/Error404/Error404';
-import Event from './pages/Event/Event.jsx'; 
+ import Event from './pages/Event/Event.jsx'; 
+
+
 
 function App() {
   return (
+ 
     <div className="App">
       <Routes>
-      <Route path="/" element={<Event />} />
-        
+   
+        <Route path="/" element={<Event />} />
         <Route path="/" element={<Login />} />
-
         <Route path="/reset-password" element={<ResetPassword />} />
-
         <Route path="*" element={<Error404 />} />
       </Routes>
     </div>
+    
   );
 }
 
 export default App;
-
